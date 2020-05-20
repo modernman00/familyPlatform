a8da21d7764b73c617207e15e6c31bf4d38df0fd
modernman00 <43863051+modernman00@users.noreply.github.com>
2020-05-20 20:38:35 +0100

    Add files via upload

---
+++ form2.js
@@ -0,0 +1,41 @@
+// function to get input value
+const idValue = (element) => {return document.getElementById(element).value}
+const getId = (element) => {return document.getElementById(element);}
+// function to return innerHTML
+const getInnerHtml = (element, html) => {return document.getElementById(element).innerHTML = html;};
+
+// function to process our form
+const showItem = (element, value) => {
+    const voalue = idValue(value)
+    if (typeof voalue == 'string') {
+        return document.getElementById(element).innerHTML = voalue.toUpperCase()
+    } else {
+        return document.getElementById(element).innerHTML = `£ ${voalue}`
+    }
+}
+
+let formInput = []
+const processForm = () => {
+    const name = idValue('name');
+    const age = idValue("age");
+    const address = idValue("address");
+    const gender = idValue("gender");
+    const email = idValue("email");
+    let no = Math.round() 
+   input = {'id': no, 'name': name, 'age': age, 'address': address, 'gender': gender, 'email': email}
+   return formInput.push(input)  
+}
+
+    const outcome = document.getElementById('submit');
+    outcome.addEventListener('click', () =>{
+        processForm()
+        console.log(formInput)
+        localStorage.setItem('formData', formInput)   
+    })
+
+    console.log(localStorage.getItem('formData'))
+
+
+
+
+
