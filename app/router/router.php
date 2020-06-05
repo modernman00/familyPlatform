79ebd6523f6770a6e2ee4c8b61c7ff22a72a32da
modernman00 <waleolaogunrac@gmail.com>
2020-06-05 21:03:36 +0100

    router

---
+++ app/router/router.php
@@ -0,0 +1,14 @@
+<?php
+
+$router = new AltoRouter;
+
+// homepage
+$router->map('GET', '/', 'App\controller\Index@index', 'Home');
+
+$router->map('GET', '/about/us', 'App\controller\About@index', 'About');
+
+$router->map('GET', '/register', 'App\controller\Register@Index', 'Register');
+
+$router->map('POST', '/register', 'App\controller\Register@submitForm', 'submit Register');
+
+$router->map('GET', '/createTable', 'App\controller\Create@index', 'create Table');
