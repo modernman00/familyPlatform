309586d60afa725c4b594392700036056ac759d0
modernman00 <waleolaogunrac@gmail.com>
2020-10-31 13:39:54 +0000

    p

--- bootstrap/cache/40b7471ccbecd890aa8e798542ec05a50f40e887.php
+++
@@ -1,9 +0,0 @@
-<?php $__env->startSection('title','about'); ?>
-
-<?php $__env->startSection('content'); ?>
-
-<h1>About the Olaogun Family</h1>
-
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('base', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\familyPlatform\resources\view/about.blade.php ENDPATH**/ ?>
--- bootstrap/cache/bd7e38767c9bda4fc0b3864e80df80b347d325f6.php
+++
@@ -1,9 +0,0 @@
-<?php $__env->startSection('title','ERROR'); ?>
-
-<?php $__env->startSection('content'); ?>
-
-<h1>THERE IS AN ERROR</h1>
-
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('base', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\familyPlatform\resources\view/error/genError.blade.php ENDPATH**/ ?>
