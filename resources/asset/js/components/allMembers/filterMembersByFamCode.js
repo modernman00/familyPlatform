03b2f7c737ef439a962ef1a48b32938d9c8c9f1b
modernman00@yahoo.com <modernman00@yahoo.com>
2024-03-30 18:05:23 +0000

    anything

--- resources/asset/js/components/allMembers/filterMembersByFamCode.js
+++ resources/asset/js/components/allMembers/filterMembersByFamCode.js
@@ -2,6 +2,8 @@ const reqId = localStorage.getItem('requesterId');
 
 
 export const filterMembersByFamCode = (data, famCode) => {
+  
+
     return data.filter(
       el => el.id !== reqId && (el.famCode === famCode || el.requesterCode === famCode)
       );
