7dbbab135b40c3fc5247adca4c4173916e298ce7
modernman00 <modernman00@yahoo.com>
2025-09-16 11:27:00 +0100

    delete deprecated files

--- resources/views/layouts/footer.blade.php
+++
@@ -1,7 +0,0 @@
-{{-- <footer class="w3-container w3-theme-d3 w3-padding-5">
-  <h5>Footer</h5>
-</footer>
-
-<footer class="w3-container w3-theme-d5">
-  <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" rel="noopener noreferrer" target="_blank">w3.css</a></p>
-</footer> --}}
