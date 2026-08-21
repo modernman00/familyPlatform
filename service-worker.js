a8ade4ac7be9e8162a24117c40359e729cd56723
modernman00@yahoo.com <modernman00@yahoo.com>
2026-08-21 11:57:16 +0100

    xx

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const Version = "1787308291832"; // Network-first default
+const Version = "1787309700494"; // Network-first default
 const CacheName = `cache-${Version}`;
 const CacheFiles = [
   "/",
